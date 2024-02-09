83c1f2162ea7ff545ac20e44778037b6463c5c94
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-09 10:07:17 -0600

    Creación de la clase Certificado

---
+++ Certificado.hpp
@@ -0,0 +1,13 @@
+#ifndef CERTIFICADO_HPP
+#define CERTIFICADO_HPP
+class Certificado
+{
+private:
+    /* data */
+public:
+    Certificado(/* args */);
+    ~Certificado();
+    void obtenerInfo();
+};
+
+#endif
