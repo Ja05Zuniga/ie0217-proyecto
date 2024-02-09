12fa174381d261acaa83fb4f7beccbe9fb709456
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-09 10:07:32 -0600

    Creación de la clase préstamos

---
+++ Prestamo.hpp
@@ -0,0 +1,13 @@
+#ifndef PRESTAMOS_HPP
+#define PRESTAMOS_HPP
+class Prestamo
+{
+private:
+    /* data */
+public:
+    Prestamo(/* args */);
+    ~Prestamo();
+    void obtenerInfo();
+};
+
+#endif
