f858ccd322cb341ce0da3519e792941f58237ee1
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-09 23:17:51 -0600

    Implementación de funcionalidades para almacenar préstamos

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -1,10 +1,29 @@
 #include "Cliente.hpp"
 
-Cliente::Cliente(unsigned int userId, std::string nombre)
+Cliente::Cliente() {}
+
+Cliente::Cliente(unsigned int userId, std::string nombre) : userId(userId), nombre(nombre)
 {
 }
 
 Cliente::~Cliente()
 {
 }
-void obtenerInfo() {}
+void Cliente::obtenerInfo()
+{
+    std::cout << "Nombre: " << nombre << "\n"
+              << "ID: " << userId << std::endl;
+}
+unsigned int Cliente::agregarPrestamo(Prestamo prestamo)
+{
+    unsigned int id = prestamo.generarId();
+    prestamos[id] = prestamo;
+    return id;
+}
+
+Prestamo Cliente::buscarPrestamo(const unsigned int id)
+{
+    return prestamos.at(id);
+}
+
+void Cliente::debitar() {}
--- src/Cliente.hpp
+++ src/Cliente.hpp
@@ -1,19 +1,24 @@
 #ifndef CLIENTE_HPP
 #define CLIENTE_HPP
 #include <string>
-
+#include <iostream>
+#include "Prestamo.hpp"
+#include <unordered_map>
 class Cliente
 {
 private:
     unsigned int userId;
     std::string nombre;
+    std::unordered_map<unsigned int, Prestamo> prestamos;
 
 public:
+    Cliente();
     Cliente(unsigned int userId, std::string nombre);
     ~Cliente();
     void obtenerInfo();
+    unsigned int agregarPrestamo(Prestamo prestamos);
+    Prestamo buscarPrestamo(const unsigned int id);
+    void debitar();
 };
-
-
 
 #endif
