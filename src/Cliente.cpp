1f2d87862235090415573f20af6d99d2e12d86d0
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-10 20:29:27 -0600

    Método para agregar préstamos y obtener información de los mismos y sus cuentas

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -4,6 +4,8 @@ Cliente::Cliente() {}
 
 Cliente::Cliente(unsigned int userId, std::string nombre) : userId(userId), nombre(nombre)
 {
+    cuentaColones = Cuenta(0, COLONES);
+    cuentaDolares = Cuenta(0, DOLARES);
 }
 
 Cliente::~Cliente()
@@ -14,16 +16,74 @@ void Cliente::obtenerInfo()
     std::cout << "Nombre: " << nombre << "\n"
               << "ID: " << userId << std::endl;
 }
-unsigned int Cliente::agregarPrestamo(Prestamo prestamo)
+unsigned int Cliente::agregarPrestamo(Prestamo prestamo, const float &monto, Moneda moneda)
 {
-    unsigned int id = prestamo.generarId();
+    Dinero dinero(monto, moneda);
+    unsigned int id = prestamo.abrir(dinero);
+    Cuenta *cuenta;
+    switch (moneda)
+    {
+    case 0:
+        cuenta = &cuentaColones;
+        break;
+    case 1:
+        cuenta = &cuentaDolares;
+        break;
+    default:
+        break;
+    }
+    Transaccion transaccion(&prestamo, cuenta, dinero);
+
+    transaccion();
     prestamos[id] = prestamo;
     return id;
 }
 
-Prestamo Cliente::buscarPrestamo(const unsigned int id)
+Prestamo Cliente::buscarPrestamo(const unsigned int &id)
 {
     return prestamos.at(id);
 }
 
+void Cliente::obtenerInfoPrestamo(const unsigned int &id)
+{
+    //    Prestamo prestamo = this->buscarPrestamo(id);
+    //    std::string tipo_str;
+    //    switch (prestamo.tipo)
+    //    {
+    //    case 0:
+    //        tipo_str = "Personal";
+    //        break;
+    //    case 1:
+    //        tipo_str = "Prendario";
+    //        break;
+    //    case 2:
+    //        tipo_str = "Hipotecario";
+    //
+    //    default:
+    //        break;
+    //    }
+    //    std::cout << "Tipo de préstamo: " << tipo_str << "\n"
+    //              << "Número de cuotas: " << prestamo.cuotas << "\n"
+    //              << "Tasa de interés anual: " << prestamo.tasaInteresAnual << std::endl
+    //              << "\n"
+    //              << "Monto: " << prestamo.montoIncial << "\n"
+    //              << "Monto pagado: " << prestamo.montoPagado << "\n";
+}
 void Cliente::debitar() {}
+
+void Cliente::obtenerEstadoCuenta(Moneda moneda)
+{
+    Cuenta *cuenta;
+    switch (moneda)
+    {
+    case 0:
+        cuenta = &cuentaColones;
+        break;
+    case 1:
+        cuenta = &cuentaDolares;
+
+    default:
+        break;
+    }
+    cuenta->obtenerInfo();
+}
--- src/Cliente.hpp
+++ src/Cliente.hpp
@@ -2,22 +2,30 @@
 #define CLIENTE_HPP
 #include <string>
 #include <iostream>
-#include "Prestamo.hpp"
 #include <unordered_map>
+#include "Prestamo.hpp"
+#include "Dinero.hpp"
+#include "Transaccion.hpp"
+#include "Cuenta.hpp"
+
 class Cliente
 {
 private:
     unsigned int userId;
     std::string nombre;
     std::unordered_map<unsigned int, Prestamo> prestamos;
+    Cuenta cuentaColones;
+    Cuenta cuentaDolares;
 
 public:
     Cliente();
     Cliente(unsigned int userId, std::string nombre);
     ~Cliente();
     void obtenerInfo();
-    unsigned int agregarPrestamo(Prestamo prestamos);
-    Prestamo buscarPrestamo(const unsigned int id);
+    unsigned int agregarPrestamo(Prestamo prestamos, const float &monto, Moneda moneda);
+    Prestamo buscarPrestamo(const unsigned int &id);
+    void obtenerInfoPrestamo(const unsigned int &id);
+    void obtenerEstadoCuenta(Moneda moneda);
     void debitar();
 };
 
