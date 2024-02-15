55507899cff621540550dc905965523c7a16ef2b
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-14 20:16:34 -0600

    Todas las constantes se agruparon en un archivo

---
+++ src/constants.hpp
@@ -0,0 +1,11 @@
+// constants.h
+#pragma once
+
+namespace Constantes
+{
+    const int COL_WIDTH = 20;
+    const int NUM_PRESTAMOS = 9;
+    const int TASA_COMPRA = 500;
+    const int TASA_VENTA = 500;
+
+} // namespace Constantes
