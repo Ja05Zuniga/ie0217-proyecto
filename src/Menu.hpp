33db960af14ee3f0ace982ee969c78233f98ba32
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-14 21:42:55 -0600

    Opción para agregar un nuevo préstamo

--- src/Menu.hpp
+++ src/Menu.hpp
@@ -8,50 +8,48 @@
 #include "agregar_cliente.hpp"
 #include "Banco.hpp"
 
+class Menu
+{
 
-class Menu{
+public:
+    // Constructor de la clase
+    Menu(Banco banco);
+    // Metodo iniciar el Menu
+    void iniciarMenu();
 
-    public:
-        // Constructor de la clase
-        Menu(Banco banco);
-        // Metodo iniciar el Menu
-        void iniciarMenu();
+protected:
+    // Metodo para obtener la identidad del usario
+    int obtenerIdentidad();
+    // Metodo para verificar si el cliente ya existe en el sistema
+    bool verificarCliente(int cedula);
+    // Metodo para crear un cliente
+    void crearCliente(int cedula);
+    // Metodo para mostrar el menu de opciones principales
+    void displayOpcionesPrincipales();
+    // Metodo para manejar las opciones del cliente
+    void gestionarCliente();
+    // Metodo para mostrar informacion general
+    void displayInformacion();
+    void displayInformacionGeneral();
+    void displayInformacionPrestamo();
+    void agregarPrestamo();
+    void pagarPrestamo();
+    // Metodos que deben ir dentro de gestionarCliente() y displayInformacion()
+    // Metodo para gestionar los Ahorros
+    void gestionarAhorros();
+    // Metodo para realizar la operacion ingresada por el usuario
+    void realizarOperaciones();
+    // Metodo para mostrar los tipos de prestamos disponibles
+    void displayTipoPrestamos();
 
-    protected:
-        // Metodo para obtener la identidad del usario
-        int obtenerIdentidad();
-        // Metodo para verificar si el cliente ya existe en el sistema
-        bool verificarCliente(int cedula);
-        // Metodo para crear un cliente
-        void crearCliente(int cedula);
-        // Metodo para mostrar el menu de opciones principales
-        void displayOpcionesPrincipales();
-        // Metodo para manejar las opciones del cliente
-        void gestionarCliente();
-        // Metodo para mostrar informacion general
-        void displayInformacion();
-        void displayInformacionGeneral();
-        void displayInformacionPrestamo();
-        
-        // Metodos que deben ir dentro de gestionarCliente() y displayInformacion()
-        // Metodo para gestionar los Ahorros
-        void gestionarAhorros();
-        // Metodo para realizar la operacion ingresada por el usuario
-        void realizarOperaciones();
-        // Metodo para mostrar los tipos de prestamos disponibles
-        void displayTipoPrestamos();
+    // // Metodo para escoger el tipo de moneda para la transaccion
+    // void escogerMoneda();
+    // // Metodo para mostrar el monto y desglose de los prestamos
+    // void displayMontoDesglosado();
 
-        // // Metodo para escoger el tipo de moneda para la transaccion
-        // void escogerMoneda();
-        // // Metodo para mostrar el monto y desglose de los prestamos
-        // void displayMontoDesglosado();
-    
-    private:
-        Banco banco;
-        Cliente cliente;
-
-
+private:
+    Banco banco;
+    Cliente cliente;
 };
-
 
 #endif
