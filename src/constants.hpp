bc0c53de2f35447132823ba9f7c7247b9e23cdec
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-15 16:34:46 -0600

    Documentación de las constantes

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,11 +1,24 @@
-// constants.h
+/**
+ * @file constants.hpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-15
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #pragma once
 
 namespace Constantes
 {
+    // Ancho de la columna de las tablas
     const int COL_WIDTH = 20;
+    // Número de préstamos ofrecidos por el banco
     const int NUM_PRESTAMOS = 9;
+    // Tasa de compra del dolar
     const int TASA_COMPRA = 500;
+    // Tasa de venta del dolar
     const int TASA_VENTA = 500;
 
 } // namespace Constantes
