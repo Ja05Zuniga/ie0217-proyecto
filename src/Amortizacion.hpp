f7e72ce601614c7855273c026f4bb8d29363bd48
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-17 15:19:19 -0600

    Código para calcular amortización

---
+++ src/Amortizacion.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file Amortizacion.hpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-15
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef AMORTIZACION_HPP
+#define AMORTIZACION_HPP
+/**
+ * @brief Estructura para almacenar datos de la amortización
+ * 
+ */
+struct Amortizacion
+{
+    int cuota;
+    float intereses;
+    float amortizacion;
+    float saldoRestante;
+};
+
+#endif
