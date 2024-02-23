d1f9421f89fa3b9b803ff9e675c6c27ec47d7fd2
Guillermo E. <guillermo.escobara3@gmail.com>
2024-02-22 21:08:31 -0600

    Actualización de tipos de cambio

--- src/constants.hpp
+++ src/constants.hpp
@@ -17,8 +17,8 @@ namespace Constantes
     // Número de préstamos ofrecidos por el banco
     const int NUM_PRESTAMOS = 9;
     // Tasa de compra del dolar
-    const int TASA_COMPRA = 500;
+    const int TASA_COMPRA = 511;
     // Tasa de venta del dolar
-    const int TASA_VENTA = 500;
+    const int TASA_VENTA = 517;
 
 } // namespace Constantes
